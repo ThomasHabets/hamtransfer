f4afb91b6798a1eb65d47d84a33ed5f71afa3075
Thomas Habets <thomas@habets.se>
2023-05-14 17:42:18 +0100

    Add half of the gRPC code.
    Remaining: need to serialize/parse in and out of UI frames.

--- src/downloader/main.rs
+++ src/downloader/main.rs
@@ -1,6 +1,19 @@
 use std::fs;
 
-fn main() {
+use tokio_stream::StreamExt;
+use tonic::transport::Channel;
+
+use ax25ms::router_service_client::RouterServiceClient;
+// use ax25ms::Frame;
+use ax25ms::StreamRequest;
+
+pub mod ax25ms {
+    // The string specified here must match the proto package name
+    tonic::include_proto!("ax25ms");
+}
+
+#[tokio::main]
+async fn main() ->  Result<(), Box<dyn std::error::Error>> {
     // Needed input:
     let source_block_size = 6;
     // let total_size = …
@@ -10,9 +23,26 @@ fn main() {
     let mut n = 0u32;
     let mut decoder = raptor_code::SourceBlockDecoder::new(source_block_size);
 
+    let mut client = RouterServiceClient::connect("http://[::1]:12001").await?;
+    let mut stream = client.stream_frames(StreamRequest{})
+        .await
+        .unwrap()
+        .into_inner();
+    
     while decoder.fully_specified() == false {
         println!("Reading symbol {}", n);
-        let encoding_symbol = fs::read(format!("tmp/{}", n)).expect("read data");
+        // 
+        // Read from file:
+        //let encoding_symbol = fs::read(format!("tmp/{}", n)).expect("read data");
+        //
+        let frame = stream.next().await.unwrap().unwrap().payload;
+
+        //
+        // TODO: parse UI frame.
+        //
+        
+        let encoding_symbol = frame;
+        
         encoding_symbol_length = encoding_symbol.len();
         let esi = n;
         decoder.push_encoding_symbol(&encoding_symbol, esi);
@@ -23,4 +53,5 @@ fn main() {
     let source_block = decoder.decode(source_block_size as usize).expect("decode");
     println!("{:?}", source_block);
     println!("{:?}", source_block.len());
+    Ok(())
 }
--- src/uploader/main.rs
+++ src/uploader/main.rs
@@ -1,4 +1,12 @@
 use std::fs;
+use ax25ms::router_service_client::RouterServiceClient;
+use ax25ms::SendRequest;
+use ax25ms::Frame;
+
+pub mod ax25ms {
+    // The string specified here must match the proto package name
+    tonic::include_proto!("ax25ms");
+}
 
 fn float_to_usize(f: f64) -> Option<usize> {
     let ret = f as usize;
@@ -18,7 +26,8 @@ fn usize_to_float(f: usize) -> Option<f64> {
     Some(ret)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() ->  Result<(), Box<dyn std::error::Error>> {
     let len = 1099;
     let mut source_data: Vec<u8> = vec![1,2,3,4,5,6,7,8,9,10,11,12];
     source_data.resize(len, 0);
@@ -30,7 +39,7 @@ fn main() {
     
     let nb_repair = 3;
 
-    let f = (usize_to_float(source_data.len()).unwrap() / 200.0).ceil();
+    let f = (usize_to_float(source_data.len()).unwrap() / usize_to_float(packet_size).unwrap()).ceil();
     let max_source_symbols = float_to_usize(f).unwrap();
 
     // State that needs to be sent:
@@ -43,7 +52,25 @@ fn main() {
     let n = encoder.nb_source_symbols() + nb_repair;
     for esi in 0..n as u32 {
         let encoding_symbol = encoder.fountain(esi);
-        println!("{:?}", encoding_symbol.len());
-        fs::write(format!("tmp/{}", esi), encoding_symbol).expect("write block");
     }
+
+    // Transmit RPC.
+    let mut client = RouterServiceClient::connect("http://[::1]:12001").await?;
+    for esi in 0..n as u32 {
+        let encoding_symbol = encoder.fountain(esi);
+        let len = encoding_symbol.len();
+        //println!("{:?}", encoding_symbol.len());
+        //fs::write(format!("tmp/{}", esi), encoding_symbol).expect("write block");
+        //
+        // TODO: construct AX.25 frame.
+        // 
+        let request = tonic::Request::new(SendRequest {
+            frame: Some(Frame {
+                payload: encoding_symbol,
+            }),
+        });
+        client.send(request).await?;
+        println!("Sent esi {} of size {}", esi, len);
+    }
+    Ok(())
 }
